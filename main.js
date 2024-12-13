871a8138a7dd8d70a90e503a1f6c1539d967d2c5
David Madison <davidmadison95@yahoo.com>
2024-12-13 17:15:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
+    // Add a small delay to ensure all elements are loaded
+    window.setTimeout(() => {
         document.body.classList.remove('not-loaded');
+        console.log('Animation started'); // This will help us debug
     }, 1000);
 });
